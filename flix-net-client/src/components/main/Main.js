0366f5dd26e9b372aec3990e529590ee0ef24f08
Darth-Revan-13 <alexander.hilbert@bryanuniversity.edu>
2021-01-03 14:37:43 -0500

    home

--- flix-net-client/src/components/main/Main.js
+++ flix-net-client/src/components/main/Main.js
@@ -4,20 +4,21 @@ import {Switch, Route} from 'react-router-dom';
 
 import Movies from './mainComponets/movies/Movies';
 import TVShows from './mainComponets/tvShows/TvShows';
+import Home from './mainComponets/home/Home';
 import {Link} from 'react-router-dom';
 
 export default function Main() {
     return (
         <main className='main'>
         <nav className='nav'>
-            {/* <Link className='link' to='/Home'>Home</Link> */}
+            <Link className='link' to='/Home'>Home</Link>
             <Link className='link' to='/Movies'>Movies</Link>
             <Link className='link' to='/TVShows'>TV Shows</Link>
         </nav>
             <Switch>
-                {/* <Route exact path='/Home'>
+                <Route exact path='/Home'>
                     <Home />
-                        </Route> */}
+                        </Route>
                 <Route exact path='/Movies'>
                     <Movies />
                         </Route>
---
+++ flix-net-client/src/components/main/mainComponets/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function Home() {
+    return (
+        <div>
+            <center><h1>Welcome to Flix Net!</h1></center>
+        </div>
+    )}
